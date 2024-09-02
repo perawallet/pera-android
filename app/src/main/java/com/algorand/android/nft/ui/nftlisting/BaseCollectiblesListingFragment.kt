c5541a9966daaf61c8f1b0de3a20d511b48715f9
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2024-09-02 03:24:11 +0300

    Remove unused class

--- app/src/main/java/com/algorand/android/nft/ui/nftlisting/BaseCollectiblesListingFragment.kt
+++ app/src/main/java/com/algorand/android/nft/ui/nftlisting/BaseCollectiblesListingFragment.kt
@@ -13,7 +13,6 @@
 package com.algorand.android.nft.ui.nftlisting
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.core.view.updatePadding
@@ -99,7 +98,6 @@ abstract class BaseCollectiblesListingFragment : DaggerBaseFragment(R.layout.fra
 
     private fun initCollectibleListingPreview(nftListingPreview: CollectiblesListingPreview) {
         with(nftListingPreview) {
-            Log.e("test", baseCollectibleListItems.toString())
             with(binding) {
                 emptyStateScrollView.isVisible = isEmptyStateVisible
                 receiveCollectiblesButton.isVisible = isReceiveButtonVisible

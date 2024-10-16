e7b59da1d3d3d2cf9da88e0938740c88d57cbacc
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2024-10-17 00:48:02 +0300

    PERA-747 Create test for url parser and caching classes

--- app/src/main/java/com/algorand/android/modules/walletconnect/cards/domain/usecase/CacheWalletConnectRequestPreselectedAccountAddressesUseCase.kt
+++ app/src/main/java/com/algorand/android/modules/walletconnect/cards/domain/usecase/CacheWalletConnectRequestPreselectedAccountAddressesUseCase.kt
@@ -12,30 +12,19 @@
 
 package com.algorand.android.modules.walletconnect.cards.domain.usecase
 
+import com.algorand.android.modules.walletconnect.cards.domain.helper.WalletConnectUrlSelectedAddressParser
 import com.algorand.android.modules.walletconnect.cards.domain.repository.WalletConnectCardsRepository
 import javax.inject.Inject
 
 internal class CacheWalletConnectRequestPreselectedAccountAddressesUseCase @Inject constructor(
-    private val walletConnectCardsRepository: WalletConnectCardsRepository
+    private val walletConnectCardsRepository: WalletConnectCardsRepository,
+    private val walletConnectUrlSelectedAddressParser: WalletConnectUrlSelectedAddressParser
 ) : CacheWalletConnectRequestPreselectedAccountAddresses {
 
     override fun invoke(wcRequestUrl: String) {
-        val address = getSelectedAccountAddress(wcRequestUrl)
+        val address = walletConnectUrlSelectedAddressParser(wcRequestUrl)
         if (address != null) {
             walletConnectCardsRepository.cachePreselectedAccountAddresses(listOf(address))
         }
-    }
-
-    private fun getSelectedAccountAddress(wcRequestUrl: String): String? {
-        return wcRequestUrl.split("?")
-            .lastOrNull()
-            ?.split("&")
-            ?.firstOrNull { it.contains(SELECTED_ACCOUNT_QUERY_KEY) }
-            ?.split("=")
-            ?.lastOrNull()
-    }
-
-    private companion object {
-        const val SELECTED_ACCOUNT_QUERY_KEY = "selectedAccount"
     }
 }

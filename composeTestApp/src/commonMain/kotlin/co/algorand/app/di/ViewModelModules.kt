9dc9f0c6546cad99450fc65b7970536d2be1daf1
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2024-11-26 03:32:29 +0300

    Refactor bottom navigation

--- composeTestApp/src/commonMain/kotlin/co/algorand/app/di/ViewModelModules.kt
+++ composeTestApp/src/commonMain/kotlin/co/algorand/app/di/ViewModelModules.kt
@@ -1,9 +1,9 @@
 package co.algorand.app.di
 
-import co.algorand.app.ui.viewmodels.AlgorandBaseViewModel
+import co.algorand.app.ui.widgets.snackbar.SnackbarViewModel
+import com.algorand.common.viewmodel.EventDelegate
 import org.koin.dsl.module
 
-val provideViewModelModules =
-    module {
-        single { AlgorandBaseViewModel() }
-    }
+val provideViewModelModules = module {
+    single { SnackbarViewModel(EventDelegate()) }
+}

33bdcac11f824fe4b02deeaa911430bfc4b4556b
Michael T Chuang <github@michaeltchuang.com>
2024-12-08 14:58:51 -0800

    keep db on version 1 till hd wallet launch or stable

--- wallet-sdk/src/commonMain/kotlin/com/algorand/common/account/local/data/database/AccountDatabase.kt
+++ wallet-sdk/src/commonMain/kotlin/com/algorand/common/account/local/data/database/AccountDatabase.kt
@@ -44,7 +44,7 @@ internal abstract class AccountDatabase : RoomDatabase() {
     abstract fun algo25Dao(): Algo25Dao
 
     companion object {
-        const val DATABASE_VERSION = 2
+        const val DATABASE_VERSION = 1
         const val DATABASE_NAME = "account_database"
     }
 }

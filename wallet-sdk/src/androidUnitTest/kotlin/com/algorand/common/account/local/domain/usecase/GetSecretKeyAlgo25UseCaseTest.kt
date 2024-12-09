90828b49dd86365c579ef7c08f4ef325e5e6c8a1
Michael T Chuang <github@michaeltchuang.com>
2024-12-09 12:04:24 -0800

    PERA-1224 :: Fix typo

--- wallet-sdk/src/androidUnitTest/kotlin/com/algorand/common/account/local/domain/usecase/GetSecretKeyAlgo25UseCaseTest.kt
+++ wallet-sdk/src/androidUnitTest/kotlin/com/algorand/common/account/local/domain/usecase/GetSecretKeyAlgo25UseCaseTest.kt
@@ -13,7 +13,7 @@
 package com.algorand.common.account.local.domain.usecase
 
 import com.algorand.common.account.local.domain.model.LocalAccount
-import com.algorand.common.account.local.domain.repository.Bip39AccountRepository
+import com.algorand.common.account.local.domain.repository.Algo25AccountRepository
 import com.algorand.common.testing.peraFixture
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -22,30 +22,30 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertNull
 import org.junit.Test
 
-class GetSecretKeyBip39UseCaseTest {
+class GetSecretKeyAlgo25UseCaseTest {
 
-    private val bip39AccountRepository: Bip39AccountRepository = mockk()
+    private val algo25AccountRepository: Algo25AccountRepository = mockk()
 
-    private val sut = GetSecretKeyBip39UseCase(bip39AccountRepository)
+    private val sut = GetSecretKeyAlgo25UseCase(algo25AccountRepository)
 
     @Test
-    fun `EXPECT secret key WHEN bip39 account is found`() = runTest {
-        coEvery { bip39AccountRepository.getAccount(BIP_39_ADDRESS) } returns BIP_39_ACCOUNT
-        val result = sut(BIP_39_ADDRESS)
-        assertTrue(result.contentEquals(BIP_39_ACCOUNT.secretKey))
+    fun `EXPECT secret key WHEN algo25 account is found`() = runTest {
+        coEvery { algo25AccountRepository.getAccount(ALGO_25_ADDRESS) } returns ALGO_25_ACCOUNT
+        val result = sut(ALGO_25_ADDRESS)
+        assertTrue(result.contentEquals(ALGO_25_ACCOUNT.secretKey))
     }
 
     @Test
-    fun `EXPECT null WHEN Bip39 account is not found`() = runTest {
-        coEvery { bip39AccountRepository.getAccount(BIP_39_ADDRESS) } returns null
+    fun `EXPECT null WHEN Algo25 account is not found`() = runTest {
+        coEvery { algo25AccountRepository.getAccount(ALGO_25_ADDRESS) } returns null
 
-        val result = sut(BIP_39_ADDRESS)
+        val result = sut(ALGO_25_ADDRESS)
 
         assertNull(result)
     }
 
     companion object {
-        private const val BIP_39_ADDRESS = "ADDRESS_1"
-        private val BIP_39_ACCOUNT = peraFixture<LocalAccount.Bip39>().copy(address = BIP_39_ADDRESS)
+        private const val ALGO_25_ADDRESS = "ADDRESS_2"
+        private val ALGO_25_ACCOUNT = peraFixture<LocalAccount.Algo25>().copy(address = ALGO_25_ADDRESS)
     }
 }

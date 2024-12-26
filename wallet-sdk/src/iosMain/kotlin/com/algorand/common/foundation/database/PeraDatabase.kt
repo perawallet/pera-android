f64cb96c2a5adb3eb3224f856c86b771bf1fede9
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2024-12-26 23:36:55 +0300

    PERA-1357 Add sqlite driver to ios db

--- wallet-sdk/src/iosMain/kotlin/com/algorand/common/foundation/database/PeraDatabase.kt
+++ wallet-sdk/src/iosMain/kotlin/com/algorand/common/foundation/database/PeraDatabase.kt
@@ -14,6 +14,7 @@ package com.algorand.common.foundation.database
 
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
@@ -27,7 +28,7 @@ internal fun getPeraDatabaseBuilder(): RoomDatabase.Builder<PeraDatabase> {
     val dbFilePath = documentDirectory() + "/${PeraDatabase.DATABASE_NAME}.db"
     return Room.databaseBuilder<PeraDatabase>(
         name = dbFilePath,
-    )
+    ).setDriver(BundledSQLiteDriver())
 }
 
 @OptIn(ExperimentalForeignApi::class)

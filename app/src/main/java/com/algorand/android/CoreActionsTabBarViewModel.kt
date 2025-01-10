80b625c80e52ca6f9fb46cc39c2f09ede0c09e2b
Michael T Chuang <github@michaeltchuang.com>
2025-01-10 07:29:41 -0800

    PERA-1364 :: Fix cards flag to show on staging testnet builds

--- app/src/main/java/com/algorand/android/CoreActionsTabBarViewModel.kt
+++ app/src/main/java/com/algorand/android/CoreActionsTabBarViewModel.kt
@@ -18,6 +18,7 @@ import com.algorand.android.BuildConfig.DISCOVER_BROWSE_DAPP_TESTNET_URL
 import com.algorand.android.BuildConfig.DISCOVER_MAINNET_URL
 import com.algorand.android.BuildConfig.DISCOVER_TESTNET_URL
 import com.algorand.android.usecase.GetIsActiveNodeTestnetUseCase
+import com.algorand.android.usecase.GetIsProductionReleaseUseCase
 import com.algorand.common.remoteconfig.domain.usecase.IMMERSVE_BUTTON_TOGGLE
 import com.algorand.common.remoteconfig.domain.usecase.IsFeatureToggleEnabled
 import com.algorand.common.remoteconfig.domain.usecase.STAKING_BUTTON_TOGGLE
@@ -28,6 +29,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CoreActionsTabBarViewModel @Inject constructor(
+    private val getIsProductionReleaseUseCase: GetIsProductionReleaseUseCase,
     private val getIsActiveNodeTestnetUseCase: GetIsActiveNodeTestnetUseCase,
     private val isFeatureToggleEnabled: IsFeatureToggleEnabled
 ) : ViewModel() {
@@ -36,7 +38,8 @@ class CoreActionsTabBarViewModel @Inject constructor(
     val viewState get() = _viewState.asStateFlow()
 
     fun initViewState() {
-        val isImmersveToggleEnabled = isFeatureToggleEnabled(IMMERSVE_BUTTON_TOGGLE) && !isConnectedToTestnet()
+        val isImmersveToggleEnabled = isFeatureToggleEnabled(IMMERSVE_BUTTON_TOGGLE) &&
+                !(isConnectedToTestnet() && isProdReleaseVariant())
         val isStakingToggleEnabled = isFeatureToggleEnabled(STAKING_BUTTON_TOGGLE)
         _viewState.value = ViewState.Content(isImmersveToggleEnabled, isStakingToggleEnabled)
     }
@@ -59,6 +62,10 @@ class CoreActionsTabBarViewModel @Inject constructor(
 
     fun isConnectedToTestnet(): Boolean {
         return getIsActiveNodeTestnetUseCase.invoke()
+    }
+
+    fun isProdReleaseVariant(): Boolean {
+        return getIsProductionReleaseUseCase.invoke()
     }
 
     sealed interface ViewState {

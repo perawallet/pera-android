427e399b1a4c74e703c8cf8d3e69488577d046e5
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-01-21 19:55:07 +0300

    Fix minify issue on common-sdk

--- common-sdk/build.gradle.kts
+++ common-sdk/build.gradle.kts
@@ -17,13 +17,6 @@ android {
         consumerProguardFiles("consumer-rules.pro")
 
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17

879ca7cacaa126829ab30f856f0d6b6d983a8ea7
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-01-30 04:27:32 +0300

    PERA-1537 Migrate ledger account selection

--- app/src/main/kotlin/com/algorand/android/modules/baseledgeraccountselection/accountselection/ui/BaseLedgerAccountSelectionFragment.kt
+++ app/src/main/kotlin/com/algorand/android/modules/baseledgeraccountselection/accountselection/ui/BaseLedgerAccountSelectionFragment.kt
@@ -14,16 +14,15 @@ package com.algorand.android.modules.baseledgeraccountselection.accountselection
 
 import android.os.Bundle
 import android.view.View
-import com.algorand.android.MainNavigationDirections
 import com.algorand.android.R
 import com.algorand.android.core.DaggerBaseFragment
-import com.algorand.android.databinding.FragmentLedgerAccountSelectionBinding
-import com.algorand.android.models.Account
-import com.algorand.android.models.AccountSelectionListItem
-import com.algorand.android.models.FragmentConfiguration
 import com.algorand.android.customviews.toolbar.buttoncontainer.model.IconButton
+import com.algorand.android.databinding.FragmentLedgerAccountSelectionBinding
+import com.algorand.android.models.FragmentConfiguration
 import com.algorand.android.models.ToolbarConfiguration
 import com.algorand.android.modules.baseledgeraccountselection.accountselection.ui.adapter.LedgerAccountSelectionAdapter
+import com.algorand.android.modules.rekey.model.AccountSelectionListItem
+import com.algorand.android.modules.rekey.model.SelectedLedgerAccounts
 import com.algorand.android.utils.viewbinding.viewBinding
 import com.google.android.material.button.MaterialButton
 
@@ -33,7 +32,7 @@ abstract class BaseLedgerAccountSelectionFragment : DaggerBaseFragment(R.layout.
 
     abstract val ledgerAccountListAdapter: LedgerAccountSelectionAdapter
 
-    abstract fun onConfirmationClick(selectedAccounts: List<Account>, allAuthAccounts: List<Account>)
+    abstract fun onConfirmationClick(selectedLedgerAccounts: SelectedLedgerAccounts)
 
     abstract fun changeToolbarTitle()
 
@@ -56,17 +55,18 @@ abstract class BaseLedgerAccountSelectionFragment : DaggerBaseFragment(R.layout.
         }
 
         override fun onAccountInfoClick(accountItem: AccountSelectionListItem.AccountItem) {
-            nav(
-                MainNavigationDirections.actionGlobalLedgerAccountInformationBottomSheet(
-                    selectedLedgerAccountSelectionListItem = accountItem,
-                    authLedgerAccountSelectionListItem = baseLedgerAccountSelectionViewModel.getAuthAccountOf(
-                        accountSelectionListItem = accountItem
-                    ),
-                    rekeyedAccountSelectionListItem = baseLedgerAccountSelectionViewModel.getRekeyedAccountOf(
-                        accountSelectionListItem = accountItem
-                    )
-                )
-            )
+            // TODO Will be implemented after ledger flow
+//            nav(
+//                MainNavigationDirections.actionGlobalLedgerAccountInformationBottomSheet(
+//                    selectedLedgerAccountSelectionListItem = accountItem,
+//                    authLedgerAccountSelectionListItem = baseLedgerAccountSelectionViewModel.getAuthAccountOf(
+//                        accountSelectionListItem = accountItem
+//                    ),
+//                    rekeyedAccountSelectionListItem = baseLedgerAccountSelectionViewModel.getRekeyedAccountOf(
+//                        accountSelectionListItem = accountItem
+//                    )
+//                )
+//            )
         }
     }
 
@@ -92,10 +92,7 @@ abstract class BaseLedgerAccountSelectionFragment : DaggerBaseFragment(R.layout.
     }
 
     private fun onConfirmationButtonClick() {
-        val selectedAccounts = baseLedgerAccountSelectionViewModel.selectedAccounts
-        val allAuthAccounts = baseLedgerAccountSelectionViewModel.allAuthAccounts
-        if (selectedAccounts.isNotEmpty() && allAuthAccounts.isNotEmpty()) {
-            onConfirmationClick(selectedAccounts, allAuthAccounts)
-        }
+        val selectedAccounts = baseLedgerAccountSelectionViewModel.getSelectedAccounts() ?: return
+        onConfirmationClick(selectedAccounts)
     }
 }

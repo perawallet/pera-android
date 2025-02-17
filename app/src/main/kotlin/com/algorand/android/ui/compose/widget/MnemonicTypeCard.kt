431c8caab2a3e402865e46eecf199aa25cd401d8
Michael T Chuang <github@michaeltchuang.com>
2025-02-16 19:12:07 -0800

    PERA-1353 :: Add in HD wallet feature flag for debug builds

--- app/src/main/kotlin/com/algorand/android/ui/compose/widget/MnemonicTypeCard.kt
+++ app/src/main/kotlin/com/algorand/android/ui/compose/widget/MnemonicTypeCard.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-
 @Composable
 fun MnemonicTypeCard(
     title: String,

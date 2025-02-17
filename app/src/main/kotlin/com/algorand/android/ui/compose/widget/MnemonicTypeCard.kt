3147ad90c3a021b4af4fb75c477cf718aefe8c4b
Michael T Chuang <github@michaeltchuang.com>
2025-02-16 23:54:02 -0800

    PERA-1353 :: Fix colors slightly

--- app/src/main/kotlin/com/algorand/android/ui/compose/widget/MnemonicTypeCard.kt
+++ app/src/main/kotlin/com/algorand/android/ui/compose/widget/MnemonicTypeCard.kt
@@ -1,14 +1,14 @@
 /*
-* Copyright 2022 Pera Wallet, LDA
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License
-*/
+ * Copyright 2022 Pera Wallet, LDA
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -32,8 +32,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.algorand.android.ui.compose.widget.PeraBodyText
-import com.algorand.android.ui.compose.widget.PeraHeadlineText
 import com.algorand.android.ui.compose.widget.PeraLinkText
+import com.algorand.android.ui.compose.widget.PeraTitleText
 
 @SuppressWarnings("LongMethod")
 @Composable
@@ -59,7 +59,7 @@ fun MnemonicTypeCard(
     ) {
         Column(Modifier.padding(all = 20.dp)) {
             Row {
-                PeraHeadlineText(text = title)
+                PeraTitleText(text = title)
                 highlighted?.let {
                     Box(
                         modifier = Modifier

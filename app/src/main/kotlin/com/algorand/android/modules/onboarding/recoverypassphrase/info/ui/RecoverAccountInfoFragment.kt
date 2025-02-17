25422e2ea998b39092bd6ff2ae07494e41421431
Michael T Chuang <github@michaeltchuang.com>
2025-02-16 17:06:34 -0800

    PERA-1353 :: Add in placeholder bottomsheet card widget before styling

--- app/src/main/kotlin/com/algorand/android/modules/onboarding/recoverypassphrase/info/ui/RecoverAccountInfoFragment.kt
+++ app/src/main/kotlin/com/algorand/android/modules/onboarding/recoverypassphrase/info/ui/RecoverAccountInfoFragment.kt
@@ -12,19 +12,31 @@
 
 package com.algorand.android.modules.onboarding.recoverypassphrase.info.ui
 
+import MnemonicTypeCard
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.BottomSheetScaffoldState
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.algorand.android.R
 import com.algorand.android.models.FragmentConfiguration
@@ -90,20 +102,60 @@ class RecoverAccountInfoFragment : BaseInfoFragment() {
     @Composable
     override fun BottomSheetContent(bottomSheetState: BottomSheetScaffoldState) {
         val coroutineScope = rememberCoroutineScope()
-
         Column(
             modifier = Modifier
                 .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.surface)
                 .padding(16.dp)
         ) {
-            Text(text = "This is the Recover Info bottom sheet content.")
-            Button(onClick = {
-                coroutineScope.launch {
-                    bottomSheetState.bottomSheetState.hide()
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = {
+                    coroutineScope.launch {
+                        bottomSheetState.bottomSheetState.hide()
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close"
+                    )
                 }
-            }) {
-                Text("Close")
+                Text(
+                    text = "Select your Mnemonic type",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
             }
+
+            MnemonicTypeCard(
+                title = "Bip39",
+                description = "New inter-operable format that enables important features like HD Wallet",
+                footer = "24 Key mnemonic keys",
+                onClick = {
+                    // onMnemonicTypeSelected("Algo25")
+                    navigateToRecoverWithPassphraseFragment()
+                    coroutineScope.launch {
+                        bottomSheetState.bottomSheetState.hide()
+                    }
+                }
+            )
+
+            MnemonicTypeCard(
+                title = "Algo25",
+                description = "Legacy format that is specific to Algorand ecosystem",
+                footer = "25 Key mnemonic keys",
+                onClick = {
+                    // onMnemonicTypeSelected("Algo25")
+                    navigateToRecoverWithPassphraseFragment()
+                    coroutineScope.launch {
+                        bottomSheetState.bottomSheetState.hide()
+                    }
+                }
+            )
         }
     }
 

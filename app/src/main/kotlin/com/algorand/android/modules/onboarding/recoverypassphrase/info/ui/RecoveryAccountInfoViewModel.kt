d17e0da021a6b4954169ac76eead08610c96f93c
Michael T Chuang <github@michaeltchuang.com>
2025-02-21 16:53:22 -0800

    PERA-1353 :: add IsOnHdWalletUseCase

--- app/src/main/kotlin/com/algorand/android/modules/onboarding/recoverypassphrase/info/ui/RecoveryAccountInfoViewModel.kt
+++ app/src/main/kotlin/com/algorand/android/modules/onboarding/recoverypassphrase/info/ui/RecoveryAccountInfoViewModel.kt
@@ -1,31 +1,16 @@
 package com.algorand.android.modules.onboarding.recoverypassphrase.info.ui
 
 import androidx.lifecycle.ViewModel
-import com.algorand.android.usecase.GetIsActiveNodeTestnetUseCase
-import com.algorand.android.usecase.GetIsProductionReleaseUseCase
-import com.algorand.wallet.remoteconfig.domain.usecase.HD_WALLET_BUTTON_TOGGLE
-import com.algorand.wallet.remoteconfig.domain.usecase.IsFeatureToggleEnabled
+import com.algorand.android.usecase.IsOnHdWalletUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class RecoveryAccountInfoViewModel @Inject constructor(
-    private val getIsProductionReleaseUseCase: GetIsProductionReleaseUseCase,
-    private val getIsActiveNodeTestnetUseCase: GetIsActiveNodeTestnetUseCase,
-    private val isFeatureToggleEnabled: IsFeatureToggleEnabled
+    private val isOnHdWalletUseCase: IsOnHdWalletUseCase,
 ) : ViewModel() {
 
     fun isHdWalletToggleEnabled(): Boolean {
-        val isHdWalletToggleEnabled = isFeatureToggleEnabled(HD_WALLET_BUTTON_TOGGLE) &&
-                !isProdReleaseVariant()
-        return isHdWalletToggleEnabled
-    }
-
-    fun isConnectedToTestnet(): Boolean {
-        return getIsActiveNodeTestnetUseCase.invoke()
-    }
-
-    fun isProdReleaseVariant(): Boolean {
-        return getIsProductionReleaseUseCase.invoke()
+        return isOnHdWalletUseCase.invoke()
     }
 }
---
+++ app/src/main/kotlin/com/algorand/android/usecase/IsOnHdWalletUseCase.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 Pera Wallet, LDA
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.algorand.android.usecase
+
+import com.algorand.wallet.remoteconfig.domain.usecase.HD_WALLET_BUTTON_TOGGLE
+import com.algorand.wallet.remoteconfig.domain.usecase.IsFeatureToggleEnabled
+import javax.inject.Inject
+
+class IsOnHdWalletUseCase @Inject constructor(
+    private val getIsProductionReleaseUseCase: GetIsProductionReleaseUseCase,
+    private val getIsActiveNodeTestnetUseCase: GetIsActiveNodeTestnetUseCase,
+    private val isFeatureToggleEnabled: IsFeatureToggleEnabled,
+) {
+
+    operator fun invoke(): Boolean {
+        val isHdWalletToggleEnabled = isFeatureToggleEnabled(HD_WALLET_BUTTON_TOGGLE) &&
+                !isProdReleaseVariant()
+        return isHdWalletToggleEnabled
+    }
+
+    fun isConnectedToTestnet(): Boolean {
+        return getIsActiveNodeTestnetUseCase.invoke()
+    }
+
+    fun isProdReleaseVariant(): Boolean {
+        return getIsProductionReleaseUseCase.invoke()
+    }
+}

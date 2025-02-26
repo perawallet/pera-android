a78c8e01875c0972f83a86103132db8eddb52458
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-02-26 11:36:36 +0300

    PERA-1697 Add comments test coverage file

--- common-sdk/test-coverage/testCoverage.sh
+++ common-sdk/test-coverage/testCoverage.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
 
+# Generate HTML report for devs
 ./gradlew koverHtmlReport
+
+# Generate XML report for coverage calculation
 ./gradlew koverXmlReport
 
 ./common-sdk/test-coverage/coverageValidator.sh

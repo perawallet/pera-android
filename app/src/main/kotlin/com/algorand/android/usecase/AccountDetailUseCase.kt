be2ce70f8e8620e5ca4c445d9768bb50d15f0bd0
Yasin <6695727+yasin-ce@users.noreply.github.com>
2025-03-04 15:27:36 +0100

    PERA-1723 :: Remove isAuthAccountInDevice and canAccountSignTransaction functions from AndroidDetailUseCase

--- app/src/main/kotlin/com/algorand/android/usecase/AccountDetailUseCase.kt
+++ app/src/main/kotlin/com/algorand/android/usecase/AccountDetailUseCase.kt
@@ -82,22 +82,6 @@ class AccountDetailUseCase @Inject constructor(
         return accountRepository.getCachedAccountDetail(publicKey)?.data?.accountInformation?.amount
     }
 
-    fun canAccountSignTransaction(publicKey: String): Boolean {
-        val account = accountManager.getAccount(publicKey)
-        return when (account?.type) {
-            Account.Type.LEDGER, Account.Type.REKEYED_AUTH -> true
-            Account.Type.STANDARD -> (account.getSecretKey() ?: byteArrayOf()).isNotEmpty()
-            Account.Type.REKEYED -> isAuthAccountInDevice(account.address)
-            Account.Type.WATCH, null -> false
-        }
-    }
-
-    private fun isAuthAccountInDevice(accountAddress: String): Boolean {
-        val accountAuthAddress = getAuthAddress(accountAddress) ?: return false
-        val authAccountDetail = getCachedAccountDetail(accountAuthAddress)?.data ?: return false
-        return canAccountSignTransaction(authAccountDetail.account.address)
-    }
-
     fun getAccount(publicKey: String): Account? {
         return accountManager.getAccount(publicKey)
     }

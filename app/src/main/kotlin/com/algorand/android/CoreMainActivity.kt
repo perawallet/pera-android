d3aa7d511238cd05c5412fd6bd4eab27a44e22f9
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-03-04 23:10:31 +0300

    PERA-1447 Delete unused files

--- app/src/main/kotlin/com/algorand/android/CoreMainActivity.kt
+++ app/src/main/kotlin/com/algorand/android/CoreMainActivity.kt
@@ -37,7 +37,6 @@ import com.algorand.android.notification.NotificationPermissionManager
 import com.algorand.android.notification.PeraNotificationManager
 import com.algorand.android.usecase.AccountDetailUseCase
 import com.algorand.android.utils.TESTNET_NETWORK_SLUG
-import com.algorand.android.utils.coremanager.LocalAccountsNameServiceManager
 import com.algorand.android.utils.coremanager.ParityManager
 import com.algorand.android.utils.extensions.hide
 import com.algorand.android.utils.extensions.show
@@ -69,9 +68,6 @@ abstract class CoreMainActivity : BaseActivity() {
 
     @Inject
     lateinit var parityManager: ParityManager
-
-    @Inject
-    lateinit var localAccountsNameServiceManager: LocalAccountsNameServiceManager
 
     @Inject
     lateinit var notificationPermissionManager: NotificationPermissionManager
@@ -123,7 +119,6 @@ abstract class CoreMainActivity : BaseActivity() {
     private fun initializeCoreManagers() {
         with(lifecycle) {
             addObserver(parityManager)
-            addObserver(localAccountsNameServiceManager)
             addObserver(notificationPermissionManager)
         }
     }

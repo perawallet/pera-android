af11aa2699b26c204dd8980dbd5e2928633b98da
Michael T Chuang <github@michaeltchuang.com>
2025-03-10 10:55:49 -0700

    PERA-1353 :: fix getSeedIdIfExistingEntropy

--- common-sdk/src/main/kotlin/com/algorand/wallet/account/local/data/repository/HdSeedRepositoryImpl.kt
+++ common-sdk/src/main/kotlin/com/algorand/wallet/account/local/data/repository/HdSeedRepositoryImpl.kt
@@ -53,12 +53,14 @@ internal class HdSeedRepositoryImpl @Inject constructor(
 
     override suspend fun getSeedIdIfExistingEntropy(entropy: ByteArray): Int? {
         val entities = hdSeedDao.getAll()
-        val encryptedEntropy = aesPlatformManager.encryptByteArray(entropy)
+
         for (entity in entities) {
-            if (encryptedEntropy.contentEquals(entity.encryptedEntropy)) {
+            val decryptedEntropy = aesPlatformManager.decryptByteArray(entity.encryptedEntropy)
+            if (entropy.contentEquals(decryptedEntropy)) {
                 return entity.seedId
             }
         }
+
         return null
     }
 

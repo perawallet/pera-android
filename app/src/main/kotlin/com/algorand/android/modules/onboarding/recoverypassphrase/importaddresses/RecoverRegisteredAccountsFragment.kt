ee55969830ea3129e0907a353513546e80e85f75
Michael T Chuang <github@michaeltchuang.com>
2025-03-10 20:29:33 -0700

    PERA-1284 :: fix loader

--- app/src/main/kotlin/com/algorand/android/modules/onboarding/recoverypassphrase/importaddresses/RecoverRegisteredAccountsFragment.kt
+++ app/src/main/kotlin/com/algorand/android/modules/onboarding/recoverypassphrase/importaddresses/RecoverRegisteredAccountsFragment.kt
@@ -17,6 +17,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -34,9 +35,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.toUpperCase
@@ -46,12 +49,14 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
+import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.algorand.android.R
 import com.algorand.android.core.DaggerBaseFragment
 import com.algorand.android.models.FragmentConfiguration
 import com.algorand.android.models.StatusBarConfiguration
 import com.algorand.android.models.ToolbarConfiguration
 import com.algorand.android.ui.compose.theme.PeraTheme
+import com.algorand.android.ui.compose.widget.AnimationLoader
 import com.algorand.android.ui.compose.widget.PeraBodyText
 import com.algorand.android.ui.compose.widget.PeraCheckbox
 import com.algorand.android.ui.compose.widget.PeraHeadlineText
@@ -132,88 +137,102 @@ class RecoverRegisteredAccountsFragment : DaggerBaseFragment(0) {
             }
         }
 
-        Scaffold { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding)
-                    .padding(start = 24.dp, end = 24.dp)
-            ) {
-                PeraHeadlineText(text = stringResource(R.string.select_address_to_add))
-                PeraBodyText(
-                    modifier = Modifier.padding(top = 10.dp),
-                    text = pluralStringResource(
-                        R.plurals.select_accounts_to_add_desc,
-                        state.registeredAccounts.size
-                    )
+        if (state.isLoading) {
+            Box {
+                AnimationLoader(
+                    modifier = Modifier.align(alignment = Alignment.Center),
+                    start = ImageVector.vectorResource(R.drawable.ic_ledger_old_export),
+                    end = ImageVector.vectorResource(R.drawable.ic_phone_new),
+                    lottie = LottieCompositionSpec.RawRes(resId = R.raw.loading_dots),
+                    description = stringResource(R.string.searching_your_accounts)
                 )
+            }
+        } else {
 
-                Row(
+            Scaffold { innerPadding ->
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 34.dp),
-                    verticalAlignment = Alignment.CenterVertically
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .padding(start = 24.dp, end = 24.dp)
                 ) {
-                    PeraTitleText(
+                    PeraHeadlineText(text = stringResource(R.string.select_address_to_add))
+                    PeraBodyText(
+                        modifier = Modifier.padding(top = 10.dp),
                         text = pluralStringResource(
-                            R.plurals.search_address_count,
+                            R.plurals.select_accounts_to_add_desc,
                             state.registeredAccounts.size
-                        ),
-                        modifier = Modifier.weight(1f)
+                        )
                     )
-                    PeraScrimText(
-                        text = stringResource(R.string.select_all)
-                    )
-                    val current = if (state.selectedAddresses.isEmpty()) {
-                        ToggleableState.Off
-                    } else if (state.selectedAddresses.size == state.registeredAccounts.size) {
-                        ToggleableState.On
-                    } else {
-                        ToggleableState.Indeterminate
-                    }
-                    PeraCheckbox(
-                        checkedState = {
-                            current
-                        },
-                        onClick = {
-                            when (current) {
-                                ToggleableState.Off,
-                                ToggleableState.Indeterminate -> {
-                                    viewModel.processIntent(RecoverRegisteredAccountsIntent.SelectAllAccounts)
-                                }
-                                ToggleableState.On -> {
-                                    viewModel.processIntent(RecoverRegisteredAccountsIntent.UnselectAllAccounts)
-                                }
-                            }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 34.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        PeraTitleText(
+                            text = pluralStringResource(
+                                R.plurals.search_address_count,
+                                state.registeredAccounts.size
+                            ),
+                            modifier = Modifier.weight(1f)
+                        )
+                        PeraScrimText(
+                            text = stringResource(R.string.select_all)
+                        )
+                        val current = if (state.selectedAddresses.isEmpty()) {
+                            ToggleableState.Off
+                        } else if (state.selectedAddresses.size == state.registeredAccounts.size) {
+                            ToggleableState.On
+                        } else {
+                            ToggleableState.Indeterminate
                         }
-                    )
-                }
-                LazyColumn(modifier = Modifier.weight(1f)) {
-                    items(state.registeredAccounts) { account ->
-                        AddressItem(
-                            account = account,
-                            onCheckedChange = { isChecked ->
-                                onIntent(
-                                    RecoverRegisteredAccountsIntent.ToggleAccountSelection(
-                                        address = account.address,
-                                        isSelected = isChecked
-                                    )
-                                )
+                        PeraCheckbox(
+                            checkedState = {
+                                current
+                            },
+                            onClick = {
+                                when (current) {
+                                    ToggleableState.Off,
+                                    ToggleableState.Indeterminate -> {
+                                        viewModel.processIntent(RecoverRegisteredAccountsIntent.SelectAllAccounts)
+                                    }
+
+                                    ToggleableState.On -> {
+                                        viewModel.processIntent(RecoverRegisteredAccountsIntent.UnselectAllAccounts)
+                                    }
+                                }
                             }
                         )
                     }
-                }
-
-                PeraPrimaryButton(
-                    onClick = {
-                        /* Handle continue */
-                    },
-                    modifier = Modifier.fillMaxWidth(),
-                    text = stringResource(R.string.continue_text),
-                    enabled = {
-                        state.selectedAddresses.isNotEmpty()
+                    LazyColumn(modifier = Modifier.weight(1f)) {
+                        items(state.registeredAccounts) { account ->
+                            AddressItem(
+                                account = account,
+                                onCheckedChange = { isChecked ->
+                                    onIntent(
+                                        RecoverRegisteredAccountsIntent.ToggleAccountSelection(
+                                            address = account.address,
+                                            isSelected = isChecked
+                                        )
+                                    )
+                                }
+                            )
+                        }
                     }
-                )
+
+                    PeraPrimaryButton(
+                        onClick = {
+                            onIntent(RecoverRegisteredAccountsIntent.ImportSelectedAccounts)
+                        },
+                        modifier = Modifier.fillMaxWidth(),
+                        text = stringResource(R.string.continue_text),
+                        enabled = {
+                            state.selectedAddresses.isNotEmpty()
+                        }
+                    )
+                }
             }
         }
     }

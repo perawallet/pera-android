1c89d1b04c138238092bc24177bb6273ab644959
Michael T Chuang <github@michaeltchuang.com>
2025-03-24 17:08:00 -0700

    PERA-1812 :: remove AESPlatformManagerImpl call

--- app/src/main/kotlin/com/algorand/android/modules/algosdk/domain/usecase/BuildKeyRegOfflineTransactionImpl.kt
+++ app/src/main/kotlin/com/algorand/android/modules/algosdk/domain/usecase/BuildKeyRegOfflineTransactionImpl.kt
@@ -16,19 +16,20 @@ import com.algorand.algosdk.sdk.Sdk
 import com.algorand.android.modules.algosdk.domain.model.OfflineKeyRegTransactionPayload
 import com.algorand.android.utils.toSuggestedParams
 import com.algorand.android.utils.toUint64
-import com.algorand.wallet.encryption.domain.manager.AESPlatformManager
+import com.algorand.wallet.analytics.domain.service.PeraExceptionLogger
 import java.math.BigInteger
 import javax.inject.Inject
 
 internal class BuildKeyRegOfflineTransactionImpl @Inject constructor(
-    private val aesPlatformManager: AESPlatformManager
+    private val peraExceptionLogger: PeraExceptionLogger
 ) : BuildKeyRegOfflineTransaction {
 
     override fun invoke(payload: OfflineKeyRegTransactionPayload): ByteArray {
         return try {
             createTransaction(payload)
         } catch (e: Exception) {
-            aesPlatformManager.clearSecretByteArrayFromMemory()
+            peraExceptionLogger.logException(e)
+            ByteArray(0)
         }
     }
 

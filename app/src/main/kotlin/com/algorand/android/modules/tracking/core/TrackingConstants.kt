3460f03388b9ed4b03ea64f47e2ab1601f504672
Michael T Chuang <github@michaeltchuang.com>
2025-04-02 14:59:48 -0700

    PERA-1828 :: Add tracking for recover account types

--- app/src/main/kotlin/com/algorand/android/modules/tracking/core/TrackingConstants.kt
+++ app/src/main/kotlin/com/algorand/android/modules/tracking/core/TrackingConstants.kt
@@ -45,6 +45,8 @@ internal object PeraClickEvent {
     const val TAP_ONBOARDING_CREATE_PASSPHRASE_SKIP = "onb_create_pass_skip_tap"
     const val TAP_ONBOARDING_WRITE_PASSPHRASE_SKIP = "onb_write_pass_skip_tap"
     const val TAP_ONBOARDING_RECOVER_PASSPHRASE_SKIP = "onb_rev_pass_skip_tap"
+    const val TAP_ONBOARDING_RECOVER_ONEKEY = "onb_createacc_recover_24"
+    const val TAP_ONBOARDING_RECOVER_ALGO25 = "onb_createacc_recover_25"
 }
 
 internal object PeraEvent {

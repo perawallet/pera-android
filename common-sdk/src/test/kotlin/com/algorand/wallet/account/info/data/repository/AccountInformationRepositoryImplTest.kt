ea6acd4c04ad5318716bd2d20363b2390b5c5dbf
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-04-25 14:02:16 +0300

    PERA-2081 Add AccountInformationRepositoryImpl missing test

--- common-sdk/src/test/kotlin/com/algorand/wallet/account/info/data/repository/AccountInformationRepositoryImplTest.kt
+++ common-sdk/src/test/kotlin/com/algorand/wallet/account/info/data/repository/AccountInformationRepositoryImplTest.kt
@@ -12,22 +12,26 @@
 
 package com.algorand.wallet.account.info.data.repository
 
+import com.algorand.test.peraFixture
 import com.algorand.wallet.account.info.data.cache.AccountInformationErrorCache
 import com.algorand.wallet.account.info.data.database.dao.AccountInformationDao
 import com.algorand.wallet.account.info.data.database.dao.AssetHoldingDao
-import com.algorand.wallet.account.info.data.mapper.model.AccountInformationMapper
 import com.algorand.wallet.account.info.data.mapper.entity.AssetHoldingEntityMapper
-import com.algorand.wallet.account.info.data.mapper.model.AssetHoldingMapper
 import com.algorand.wallet.account.info.data.mapper.entity.AssetStatusEntityMapper
+import com.algorand.wallet.account.info.data.mapper.model.AccountAssetAndAppsCountMapper
+import com.algorand.wallet.account.info.data.mapper.model.AccountInformationMapper
+import com.algorand.wallet.account.info.data.mapper.model.AssetHoldingMapper
+import com.algorand.wallet.account.info.data.model.AccountAssetAndAppsCountDto
 import com.algorand.wallet.account.info.data.service.AccountInformationApiService
+import com.algorand.wallet.account.info.domain.model.AccountAssetAndAppsCount
 import com.algorand.wallet.account.local.domain.usecase.GetLocalAccountsAddresses
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import java.math.BigInteger
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import kotlinx.coroutines.test.runTest
 
 class AccountInformationRepositoryImplTest {
 
@@ -40,6 +44,7 @@ class AccountInformationRepositoryImplTest {
     private val assetHoldingEntityMapper: AssetHoldingEntityMapper = mockk()
     private val accountInformationErrorCache: AccountInformationErrorCache = mockk()
     private val getLocalAccountsAddresses: GetLocalAccountsAddresses = mockk()
+    private val accountAssetAndAppsCountMapper: AccountAssetAndAppsCountMapper = mockk()
     private val sut = AccountInformationRepositoryImpl(
         indexerApi,
         accountInformationMapper,
@@ -51,7 +56,8 @@ class AccountInformationRepositoryImplTest {
         assetStatusEntityMapper,
         assetHoldingEntityMapper,
         accountInformationErrorCache,
-        getLocalAccountsAddresses
+        getLocalAccountsAddresses,
+        accountAssetAndAppsCountMapper
     )
 
     @Test
@@ -90,5 +96,30 @@ class AccountInformationRepositoryImplTest {
         val result = sut.getAccountAlgoBalance("address")
 
         assertEquals(BigInteger.TWO, result)
+    }
+
+    @Test
+    fun `EXPECT account asset and app count WHEN exists in cache`() = runTest {
+        val dto = peraFixture<AccountAssetAndAppsCountDto>()
+        val assetAndAppsCount = peraFixture<AccountAssetAndAppsCount>()
+        coEvery { accountInformationDao.getAssetsAndAppsCount(ADDRESS) } returns dto
+        coEvery { accountAssetAndAppsCountMapper.map(dto) } returns assetAndAppsCount
+
+        val result = sut.getAccountAssetsAndAppsCount(ADDRESS)
+
+        assertEquals(assetAndAppsCount, result)
+    }
+
+    @Test
+    fun `EXPECT null WHEN asset and app count is not in cache`() = runTest {
+        coEvery { accountInformationDao.getAssetsAndAppsCount(ADDRESS) } returns null
+
+        val result = sut.getAccountAssetsAndAppsCount(ADDRESS)
+
+        assertEquals(null, result)
+    }
+
+    private companion object {
+        const val ADDRESS = "address"
     }
 }

65970caab688e8a0e756ebf976d424ed8db8b5b9
Yasin <6695727+yasin-ce@users.noreply.github.com>
2025-05-12 19:04:14 +0200

    PERA-2148 :: Add placeholder viewholder to remove asset adapter

--- app/src/main/kotlin/com/algorand/android/ui/asset/remove/view/adapter/RemoveAssetAdapter.kt
+++ app/src/main/kotlin/com/algorand/android/ui/asset/remove/view/adapter/RemoveAssetAdapter.kt
@@ -18,13 +18,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.algorand.android.models.BaseDiffUtil
 import com.algorand.android.models.BaseViewHolder
 import com.algorand.android.modules.accountdetail.assets.ui.adapter.AccountAssetsAdapter
-import com.algorand.android.ui.asset.remove.view.adapter.RemoveAssetItemViewHolder.AssetRemovalItemListener
-import com.algorand.android.ui.asset.remove.view.adapter.RemoveCollectibleItemViewHolder.CollectibleRemovalItemListener
 import com.algorand.android.ui.asset.remove.model.BaseRemoveAssetItem
+import com.algorand.android.ui.asset.remove.model.BaseRemoveAssetItem.ItemType.PLACEHOLDER_ITEM
 import com.algorand.android.ui.asset.remove.model.BaseRemoveAssetItem.ItemType.REMOVE_ASSET_ITEM
 import com.algorand.android.ui.asset.remove.model.BaseRemoveAssetItem.ItemType.REMOVE_COLLECTIBLE_ITEM
 import com.algorand.android.ui.asset.remove.model.BaseRemoveAssetItem.ItemType.SCREEN_STATE_ITEM
 import com.algorand.android.ui.asset.remove.model.BaseRemoveAssetItem.RemoveAssetItem
+import com.algorand.android.ui.asset.remove.view.adapter.RemoveAssetItemViewHolder.AssetRemovalItemListener
+import com.algorand.android.ui.asset.remove.view.adapter.RemoveCollectibleItemViewHolder.CollectibleRemovalItemListener
 
 class RemoveAssetAdapter(
     private val listener: RemoveAssetAdapterListener
@@ -51,7 +52,8 @@ class RemoveAssetAdapter(
     }
 
     override fun getItemViewType(position: Int): Int {
-        return getItem(position)?.itemType?.value ?: RecyclerView.NO_POSITION
+        if (position == RecyclerView.NO_POSITION || position >= itemCount) return PLACEHOLDER_ITEM.value
+        return getItem(position)?.itemType?.value ?: PLACEHOLDER_ITEM.value
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<BaseRemoveAssetItem> {
@@ -59,6 +61,7 @@ class RemoveAssetAdapter(
             REMOVE_ASSET_ITEM.value -> RemoveAssetItemViewHolder.create(parent, assetRemovalItemListener)
             REMOVE_COLLECTIBLE_ITEM.value -> RemoveCollectibleItemViewHolder.create(parent, collectibleItemListener)
             SCREEN_STATE_ITEM.value -> ScreenStateViewHolder.create(parent)
+            PLACEHOLDER_ITEM.value -> RemoveAssetPagingPlaceholderViewHolder.create(parent)
             else -> throw IllegalArgumentException("$logTag: Unknown viewType = $viewType")
         }
     }

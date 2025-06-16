4e65c0b3f78ef38032c6c372bc7c1e24056a2562
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-06-17 00:26:43 +0300

    PERA-2370 Display passphrase acknowledge sheet before passphrase screen

--- app/src/main/kotlin/com/algorand/android/ui/compose/widget/PeraTriStateCheckbox.kt
+++ app/src/main/kotlin/com/algorand/android/ui/compose/widget/PeraTriStateCheckbox.kt
@@ -2,48 +2,49 @@ package com.algorand.android.ui.compose.widget
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.algorand.android.ui.compose.theme.PeraTheme
 
 @Composable
-fun PeraCheckbox(
+fun PeraTriStateCheckbox(
     modifier: Modifier = Modifier,
-    checked: Boolean,
+    checkedState: () -> ToggleableState = { ToggleableState.Indeterminate },
     interactionSource: MutableInteractionSource? = MutableInteractionSource(),
-    enabled: () -> Boolean = { true },
-    onCheckChanged: (Boolean) -> Unit
+    onClick: () -> Unit,
+    enabled: () -> Boolean = { true }
 ) {
-    Checkbox(
+    TriStateCheckbox(
         interactionSource = interactionSource,
         modifier = modifier,
+        state = checkedState(),
         colors = CheckboxDefaults.colors(
             checkmarkColor = PeraTheme.colors.status.successCheckmark,
             checkedColor = PeraTheme.colors.status.success,
             uncheckedColor = PeraTheme.colors.layer.gray
         ),
-        checked = checked,
-        enabled = enabled(),
-        onCheckedChange = onCheckChanged
+        onClick = onClick,
+        enabled = enabled()
     )
 }
 
 @PreviewLightDark
 @Composable
-fun PeraCheckboxPreview() {
+fun PeraTriStateCheckboxPreview() {
     val interactionSource = remember { MutableInteractionSource() }
-    val checkedState = remember { mutableStateOf(true) }
+    val checkedState = remember { mutableStateOf(ToggleableState.Indeterminate) }
     PeraTheme {
-        PeraCheckbox(
+        PeraTriStateCheckbox(
             Modifier.background(color = PeraTheme.colors.background.primary),
-            onCheckChanged = { checkedState.value = it },
+            checkedState = { checkedState.value },
             interactionSource = interactionSource,
-            checked = checkedState.value
+            onClick = { }
         )
     }
 }

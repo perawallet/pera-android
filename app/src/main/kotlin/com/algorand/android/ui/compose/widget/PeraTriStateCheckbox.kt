442df3a8b21621377a83b069ebfbbbb965917303
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-06-17 17:00:31 +0300

    PERA-2370 Fix linter issue

--- app/src/main/kotlin/com/algorand/android/ui/compose/widget/PeraTriStateCheckbox.kt
+++ app/src/main/kotlin/com/algorand/android/ui/compose/widget/PeraTriStateCheckbox.kt
@@ -9,7 +9,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
- 
+
 package com.algorand.android.ui.compose.widget
 
 import androidx.compose.foundation.background

2c5481e9c19928fba4be8e811e9274b98bafa3d1
Yasin <6695727+yasin-ce@users.noreply.github.com>
2025-07-04 12:22:30 +0200

    PERA-2361 :: Enable discover on staging testnet (#350)

--- app/src/main/kotlin/com/algorand/android/CoreMainActivity.kt
+++ app/src/main/kotlin/com/algorand/android/CoreMainActivity.kt
@@ -44,14 +44,15 @@ import com.algorand.android.utils.coremanager.ParityManager
 import com.algorand.android.utils.extensions.collectLatestOnLifecycle
 import com.algorand.android.utils.extensions.hide
 import com.algorand.android.utils.extensions.show
+import com.algorand.android.utils.isStagingApp
 import com.algorand.android.utils.navigateSafe
 import com.algorand.android.utils.setupWithNavController
 import com.algorand.android.utils.showDarkStatusBarIcons
 import com.algorand.android.utils.showLightStatusBarIcons
 import com.algorand.android.utils.viewbinding.viewBinding
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.properties.Delegates
-import kotlinx.coroutines.launch
 
 abstract class CoreMainActivity : BaseActivity() {
 
@@ -238,7 +239,7 @@ abstract class CoreMainActivity : BaseActivity() {
     private fun handleBottomBarNavigationForChosenNetwork() {
         binding.bottomNavigationView.menu.forEach { menuItem ->
             if (menuItem.itemId == R.id.discoverHomeNavigation) {
-                menuItem.isEnabled = isConnectedToTestNet.not()
+                menuItem.isEnabled = isConnectedToTestNet.not() || isStagingApp()
             }
         }
     }

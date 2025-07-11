ba63e3809d6097a6a2a224a9f65f627bcd01b1d4
Mithat Sinan Sarı <m.sinan.sari@gmail.com>
2025-07-11 20:25:28 +0300

    PERA-2367 :: Implement asset detail price history chart (#356)

--- common-sdk/src/main/kotlin/com/algorand/wallet/remoteconfig/domain/usecase/IsFeatureToggleEnabled.kt
+++ common-sdk/src/main/kotlin/com/algorand/wallet/remoteconfig/domain/usecase/IsFeatureToggleEnabled.kt
@@ -19,6 +19,7 @@ const val DISCOVER_V5_TOGGLE = "enable_discover_v5"
 const val ENABLE_ACCOUNT_DB_MIGRATION_VIEWER = "enable_account_migration_viewer"
 const val ACCOUNTS_CHART_TOGGLE = "enable_charts_portfolio"
 const val ACCOUNT_DETAIL_CHART_TOGGLE = "enable_charts_accounts"
+const val ASSET_DETAIL_CHART_TOGGLE = "enable_charts_assets"
 
 fun interface IsFeatureToggleEnabled {
     operator fun invoke(featureToggleKey: String): Boolean

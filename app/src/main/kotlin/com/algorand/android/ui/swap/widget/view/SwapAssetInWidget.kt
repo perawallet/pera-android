9e09f4b50c481196eeb2a6dae10404c0ca191810
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-08-07 02:49:03 +0300

    PERA-2609 Create swap asset in and out widgets

---
+++ app/src/main/kotlin/com/algorand/android/ui/swap/widget/view/SwapAssetInWidget.kt
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2022-2025 Pera Wallet, LDA
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.algorand.android.ui.swap.widget.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.algorand.android.R
+import com.algorand.android.models.AnnotatedString
+import com.algorand.android.modules.currency.domain.model.Currency
+import com.algorand.android.ui.compose.theme.PeraTheme
+import com.algorand.android.ui.compose.widget.ShimmerTextBox
+import com.algorand.android.ui.compose.widget.textfield.AmountInputTextField
+import com.algorand.android.ui.compose.widget.textfield.DecimalFormattedVisualTransformation
+import com.algorand.android.ui.swap.widget.viewmodel.SwapAssetSelectionViewModel
+import com.algorand.android.ui.swap.widget.viewmodel.SwapWidgetViewModel
+import com.algorand.android.ui.swap.widget.viewmodel.SwapWidgetViewModel.ViewState
+import com.algorand.android.ui.swap.widget.viewmodel.SwapWidgetViewModel.ViewState.Content
+import com.algorand.android.ui.swap.widget.viewmodel.SwapWidgetViewModel.ViewState.Loading
+import com.algorand.android.utils.getXmlStyledString
+import com.algorand.android.utils.toBigDecimalOrZero
+import com.algorand.wallet.swap.domain.model.SwapQuoteDetail
+import com.algorand.wallet.swap.domain.model.SwapQuoteException.InsufficientAlgoBalance
+import com.algorand.wallet.swap.domain.model.SwapQuoteException.InsufficientAssetBalance
+import com.algorand.wallet.swap.domain.model.SwapQuoteException.InsufficientBalanceForFee
+
+@Composable
+fun SwapAssetInWidget(
+    widgetViewModel: SwapWidgetViewModel,
+    assetSelectionViewModel: SwapAssetSelectionViewModel
+) {
+    Column {
+        val viewState = widgetViewModel.state.collectAsStateWithLifecycle().value
+        SwapAssetWidget(
+            title = stringResource(R.string.you_pay),
+            amountContent = { AssetInAmountContent(viewState, widgetViewModel) },
+            viewModel = assetSelectionViewModel,
+            assetSelectionChipBackgroundColor = PeraTheme.colors.layer.grayLightest
+        )
+        ErrorText(viewState)
+    }
+}
+
+@Composable
+private fun RowScope.AssetInAmountContent(viewState: ViewState, widgetViewModel: SwapWidgetViewModel) {
+    var assetInAmount by remember { mutableStateOf("") }
+    Column(modifier = Modifier.weight(1f)) {
+        AssetInAmountInputTextField(viewState, assetInAmount) {
+            assetInAmount = it
+            widgetViewModel.setAmountInput(it.toBigDecimalOrZero())
+        }
+        SecondaryAmountText(viewState)
+    }
+}
+
+@Composable
+private fun AssetInAmountInputTextField(viewState: ViewState, text: String, onTextChange: (String) -> Unit) {
+    AmountInputTextField(
+        text = text,
+        hint = (viewState as? Content)?.amountRenderers?.assetInPrimaryAmountHint?.getDisplayValue(),
+        onTextChanged = onTextChange,
+        textStyle = PeraTheme.typography.body.large.sansMedium,
+        visualTransformation = DecimalFormattedVisualTransformation()
+    )
+}
+
+@Composable
+private fun SecondaryAmountText(viewState: ViewState) {
+    val textStyle = PeraTheme.typography.footnote.sans
+    when (viewState) {
+        is Content -> {
+            Text(
+                text = viewState.amountRenderers.assetInSecondaryAmount.getDisplayValue(),
+                style = textStyle,
+                color = PeraTheme.colors.text.gray
+            )
+        }
+        Loading -> ShimmerTextBox(textStyle, width = 80.dp)
+    }
+}
+
+@Composable
+private fun ErrorText(viewState: ViewState) {
+    val viewQuoteState = ((viewState as? Content)?.contentState as? Content.ContentState.Quote) ?: return
+    val quoteState = viewQuoteState.selectedQuoteDetail.state
+    if (quoteState is SwapQuoteDetail.SwapQuoteState.NonSwappable) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_error),
+                tint = PeraTheme.colors.helper.negative,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = getErrorMessage(quoteState),
+                style = PeraTheme.typography.footnote.sansMedium,
+                color = PeraTheme.colors.helper.negative
+            )
+        }
+    }
+}
+
+@Composable
+private fun getErrorMessage(quoteState: SwapQuoteDetail.SwapQuoteState.NonSwappable): String {
+    return when (quoteState.exception) {
+        InsufficientAlgoBalance -> stringResource(R.string.account_does_not_have)
+        is InsufficientAssetBalance -> {
+            val assetUnitName = (quoteState.exception as InsufficientAssetBalance).assetUnitName
+            if (!assetUnitName.isNullOrBlank()) {
+                val annotatedString = AnnotatedString(
+                    stringResId = R.string.asa_balance_is_not_sufficient_formatted,
+                    replacementList = listOf("asa_short_name" to assetUnitName)
+                )
+                LocalContext.current.getXmlStyledString(annotatedString).toString()
+            } else {
+                stringResource(R.string.asa_balance_is_not_sufficient)
+            }
+        }
+        is InsufficientBalanceForFee -> {
+            val minBalance = (quoteState.exception as InsufficientBalanceForFee).minRequiredBalance
+            val annotatedString = AnnotatedString(
+                stringResId = R.string.algo_balance_is_too_low,
+                replacementList = listOf("min_balance" to "${Currency.ALGO.symbol} $minBalance")
+            )
+            LocalContext.current.getXmlStyledString(annotatedString).toString()
+        }
+    }
+}
---
+++ app/src/main/kotlin/com/algorand/android/ui/swap/widget/view/SwapAssetSelectionChipButton.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2022-2025 Pera Wallet, LDA
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.algorand.android.ui.swap.widget.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.algorand.android.R
+import com.algorand.android.ui.compose.theme.PeraTheme
+import com.algorand.android.ui.compose.widget.asset.AssetIcon
+import com.algorand.android.ui.compose.widget.asset.AssetIconDrawable.AlgoDrawable
+import com.algorand.android.ui.compose.widget.asset.AssetIconDrawable.AssetDrawable
+import com.algorand.android.ui.swap.widget.viewmodel.SwapAssetSelectionViewModel.ViewState
+import com.algorand.wallet.asset.domain.util.AssetConstants.ALGO_ID
+import com.algorand.wallet.swap.domain.model.SwapSelectedAssetDetail
+
+@Composable
+fun SwapAssetSelectionChipButton(viewState: ViewState, backgroundColor: Color) {
+    Row(
+        modifier = Modifier
+            .background(color = backgroundColor, shape = RoundedCornerShape(16.dp))
+            .padding(horizontal = 12.dp)
+            .defaultMinSize(minHeight = 48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        when (viewState) {
+            ViewState.Idle -> Unit
+            ViewState.Error -> ErrorContent()
+            ViewState.Loading -> LoadingContent()
+            is ViewState.Content -> AssetContent(viewState.assetDetail)
+        }
+    }
+}
+
+@Composable
+private fun AssetContent(assetDetail: SwapSelectedAssetDetail) {
+    val assetDrawable = remember {
+        with(assetDetail) {
+            if (assetId == ALGO_ID) AlgoDrawable else AssetDrawable(imageUrl.orEmpty(), unitName)
+        }
+    }
+    AssetIcon(
+        modifier = Modifier.size(24.dp),
+        drawable = assetDrawable
+    )
+    Spacer(modifier = Modifier.width(6.dp))
+    Text(
+        text = assetDetail.unitName.orEmpty(),
+        style = PeraTheme.typography.body.regular.sans,
+        color = PeraTheme.colors.text.main
+    )
+    Spacer(modifier = Modifier.width(6.dp))
+    Icon(
+        modifier = Modifier.size(24.dp),
+        painter = painterResource(R.drawable.ic_right_arrow),
+        contentDescription = null,
+        tint = PeraTheme.colors.text.gray
+    )
+}
+
+@Composable
+private fun ErrorContent() {
+    Text(
+        text = stringResource(R.string.choose_an_asset),
+        style = PeraTheme.typography.body.regular.sans,
+        color = PeraTheme.colors.text.main
+    )
+    Spacer(modifier = Modifier.width(6.dp))
+    Icon(
+        modifier = Modifier.size(24.dp),
+        painter = painterResource(R.drawable.ic_right_arrow),
+        contentDescription = null
+    )
+}
+
+@Composable
+private fun LoadingContent() {
+    // TODO Shimmer
+}

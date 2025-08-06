6ef2d6cc93f37142b8ace6cb282fd585e979637b
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-08-06 22:41:31 +0300

    PERA-2609 Create swap widget related view model

---
+++ app/src/main/kotlin/com/algorand/android/ui/swap/widget/viewmodel/DefaultSwapAssetSelectionViewModel.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2022-2025 Pera Wallet, LDA
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.algorand.android.ui.swap.widget.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.algorand.android.modules.currency.domain.model.Currency
+import com.algorand.android.ui.common.amount.AmountRenderer
+import com.algorand.android.ui.common.amount.DecimalConfig
+import com.algorand.android.ui.common.amount.DecimalConfig.MinDecimalType.Fixed
+import com.algorand.android.ui.common.amount.PeraAmount
+import com.algorand.android.ui.common.amount.PlainFormattedAmount.SimplePlainFormattedAmount
+import com.algorand.android.ui.swap.widget.viewmodel.SwapAssetSelectionViewModel.ViewState
+import com.algorand.wallet.asset.domain.util.AssetConstants.ALGO_ID
+import com.algorand.wallet.swap.domain.model.SwapSelectedAssetDetail
+import com.algorand.wallet.swap.domain.usecase.GetSelectedSwapAssetDetail
+import com.algorand.wallet.viewmodel.StateDelegate
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.math.BigDecimal
+import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onStart
+
+@HiltViewModel
+class DefaultSwapAssetSelectionViewModel @Inject constructor(
+    private val getSelectedSwapAssetDetail: GetSelectedSwapAssetDetail,
+    private val stateDelegate: StateDelegate<ViewState>
+) : ViewModel(), SwapAssetSelectionViewModel {
+
+    init {
+        stateDelegate.setDefaultState(ViewState.Idle)
+    }
+
+    override val state: StateFlow<ViewState>
+        get() = stateDelegate.state
+
+    override fun initAssetDetail(addressFlow: Flow<String?>, assetIdFlow: Flow<Long>) {
+        combine(addressFlow, assetIdFlow) { address, assetId ->
+            if (address.isNullOrBlank()) {
+                stateDelegate.updateState { ViewState.Loading }
+            } else {
+                getSelectedSwapAssetDetail(address, assetId).use(
+                    onSuccess = { assetDetail ->
+                        stateDelegate.updateState { ViewState.Content(assetDetail, getBalanceRenderer(assetDetail)) }
+                    },
+                    onFailed = { _, _ ->
+                        stateDelegate.updateState { ViewState.Error }
+                    }
+                )
+            }
+        }
+            .onStart { stateDelegate.updateState { ViewState.Loading } }
+            .launchIn(viewModelScope)
+    }
+
+    private fun getBalanceRenderer(assetDetail: SwapSelectedAssetDetail): AmountRenderer {
+        val balance = PeraAmount(BigDecimal(assetDetail.amount).movePointLeft(assetDetail.decimal))
+        val decimalConfig = DecimalConfig(maxDecimals = assetDetail.decimal, minDecimals = Fixed(2))
+        val formatter = SimplePlainFormattedAmount(balance, decimalConfig)
+        return AmountRenderer(
+            formattedAmount = formatter,
+            type = AmountRenderer.RenderType.Plain,
+            prefix = Currency.ALGO.symbol.takeIf { assetDetail.assetId == ALGO_ID },
+            suffix = assetDetail.unitName.takeIf { assetDetail.assetId != ALGO_ID }
+        )
+    }
+}
---
+++ app/src/main/kotlin/com/algorand/android/ui/swap/widget/viewmodel/SwapWidgetViewModel.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022-2025 Pera Wallet, LDA
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.algorand.android.ui.swap.widget.viewmodel
+
+import com.algorand.android.ui.common.amount.AmountRenderer
+import com.algorand.android.ui.swap.widget.viewmodel.SwapWidgetViewModel.ViewState
+import com.algorand.wallet.swap.domain.model.SwapQuoteDetail
+import com.algorand.wallet.viewmodel.StateViewModel
+import java.math.BigDecimal
+import kotlinx.coroutines.flow.Flow
+
+interface SwapWidgetViewModel : StateViewModel<ViewState> {
+
+    fun setAmountInput(amount: BigDecimal?)
+
+    fun initWidget(
+        addressFlow: Flow<String?>,
+        assetInFlow: Flow<SwapAssetSelectionViewModel.ViewState>,
+        assetOutFlow: Flow<SwapAssetSelectionViewModel.ViewState>
+    )
+
+    sealed interface ViewState {
+
+        data object Loading : ViewState
+        data class Content(
+            val amountRenderers: AmountRenderers,
+            val contentState: ContentState
+        ) : ViewState {
+            sealed interface ContentState {
+                data object Idle : ContentState
+
+                data class Error(val message: String?) : ContentState
+
+                data class Quote(
+                    val selectedQuoteId: Long,
+                    val bestOfferQuoteId: Long,
+                    val quotes: List<SwapQuoteDetail>
+                ) : ContentState {
+
+                    val selectedQuoteDetail
+                        get() = quotes.first { it.quote.quoteId == selectedQuoteId }
+                }
+            }
+
+            data class AmountRenderers(
+                val assetInPrimaryAmountHint: AmountRenderer,
+                val assetInSecondaryAmount: AmountRenderer,
+                val assetOutPrimaryAmount: AmountRenderer,
+                val assetOutSecondaryAmount: AmountRenderer,
+            )
+        }
+    }
+}

d171875f819b825b8320fb202072c77ccfc9ae5e
Mithat Sinan Sarı <m.sinan.sari@gmail.com>
2025-08-13 14:32:21 +0300

    PERA-2610 :: Swap address selection screen (#384)

--- app/src/main/kotlin/com/algorand/android/ui/swap/widget/view/SwapAssetSelectionChipButton.kt
+++ app/src/main/kotlin/com/algorand/android/ui/swap/widget/view/SwapAssetSelectionChipButton.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -63,10 +62,8 @@ fun SwapAssetSelectionChipButton(viewState: ViewState, backgroundColor: Color, o
 
 @Composable
 private fun AssetContent(assetDetail: SwapSelectedAssetDetail) {
-    val assetDrawable = remember {
-        with(assetDetail) {
-            if (assetId == ALGO_ID) AlgoDrawable else AssetDrawable(imageUrl.orEmpty(), unitName)
-        }
+    val assetDrawable = with(assetDetail) {
+        if (assetId == ALGO_ID) AlgoDrawable else AssetDrawable(imageUrl.orEmpty(), unitName)
     }
     AssetIcon(
         modifier = Modifier.size(24.dp),

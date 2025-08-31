556e052f6a84e263b89c4ed06f32a268a929570f
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2025-08-31 21:37:43 +0300

    PERA-2701 Create statistic composable

---
+++ app/src/main/kotlin/com/algorand/android/ui/asset/detail/view/markets/AssetMarketsStatistic.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2022-2025 Pera Wallet, LDA
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.algorand.android.ui.asset.detail.view.markets
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.algorand.android.R
+import com.algorand.android.ui.asset.detail.model.AssetMarketsDetail
+import com.algorand.android.ui.compose.theme.PeraTheme
+import com.algorand.android.ui.compose.widget.modifier.clickableNoRipple
+
+@Composable
+fun AssetMarketsStatistics(statistics: AssetMarketsDetail.Statistics, onTotalSupplyInfoClick: () -> Unit) {
+    Column {
+        AssetMarketsSectionTitle(stringResource(R.string.statistics))
+        Spacer(modifier = Modifier.height(24.dp))
+        Row {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(R.string.price),
+                    style = PeraTheme.typography.footnote.sans,
+                    color = PeraTheme.colors.text.gray
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = statistics.price.getDisplayValue(),
+                    style = PeraTheme.typography.body.large.sansMedium,
+                    color = PeraTheme.colors.text.main
+                )
+            }
+            if (!statistics.totalSupply.isNullOrBlank()) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = stringResource(R.string.total_supply),
+                            style = PeraTheme.typography.footnote.sans,
+                            color = PeraTheme.colors.text.gray
+                        )
+                        Spacer(modifier = Modifier.width(6.dp))
+                        Icon(
+                            modifier = Modifier
+                                .clickableNoRipple(onClick = onTotalSupplyInfoClick)
+                                .size(20.dp),
+                            painter = painterResource(R.drawable.ic_info),
+                            contentDescription = null,
+                            tint = PeraTheme.colors.text.grayLighter
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = statistics.totalSupply,
+                        style = PeraTheme.typography.body.large.sansMedium,
+                        color = PeraTheme.colors.text.main
+                    )
+                }
+            }
+        }
+    }
+}

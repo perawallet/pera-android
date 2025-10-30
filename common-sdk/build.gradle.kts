6d8b3cd0243eccb60df75499036830a916f2b4b5
Yasin <6695727+yasin-ce@users.noreply.github.com>
2025-10-30 14:50:18 +0100

    PERA-3085 :: Fix jna not found issue

--- common-sdk/build.gradle.kts
+++ common-sdk/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
 
     api(libs.algosdk)
     api(files("../libs/dP256.jar"))
+    api("net.java.dev.jna:jna:5.18.1@aar")
     
     implementation(libs.algorand.go.mobile)
     implementation(libs.xhdwalletapi)

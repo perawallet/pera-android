938b9a8378ca44236a49e6fb3cfe01ad5b9832cb
Yasin <6695727+yasin-ce@users.noreply.github.com>
2026-03-12 18:00:27 +0200

    PERA-3839 :: Fix rekeyed account login issue on cards

--- app/src/test/kotlin/com/algorand/android/modules/walletconnect/domain/usecase/CreateWalletConnectArbitraryDataSignerUseCaseTest.kt
+++ app/src/test/kotlin/com/algorand/android/modules/walletconnect/domain/usecase/CreateWalletConnectArbitraryDataSignerUseCaseTest.kt
@@ -17,26 +17,26 @@ import com.algorand.android.models.WalletConnectArbitraryDataSigner.DisplayOnly
 import com.algorand.android.modules.walletconnect.domain.WalletConnectErrorProvider
 import com.algorand.android.modules.walletconnect.domain.model.WalletConnectError
 import com.algorand.test.peraFixture
-import com.algorand.wallet.account.core.domain.model.TransactionSigner
-import com.algorand.wallet.account.core.domain.model.TransactionSigner.SignerNotFound
-import com.algorand.wallet.account.core.domain.usecase.GetTransactionSigner
+import com.algorand.wallet.account.local.domain.model.LocalAccount
+import com.algorand.wallet.account.local.domain.usecase.GetLocalAccount
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class CreateWalletConnectArbitraryDataSignerUseCaseTest {
 
-    private val getTransactionSigner: GetTransactionSigner = mockk()
+    private val getLocalAccount: GetLocalAccount = mockk()
     private val errorProvider: WalletConnectErrorProvider = mockk {
         every { getUnableToSignError() } returns UNABLE_TO_SIGN_ERROR
         every { getMissingSignerError() } returns MISSING_SIGNER_ERROR
+        every { getMultisigTransactionError() } returns MULTISIG_TRANSACTION_ERROR
     }
 
-    private val sut = CreateWalletConnectArbitraryDataSignerUseCase(getTransactionSigner, errorProvider)
+    private val sut = CreateWalletConnectArbitraryDataSignerUseCase(getLocalAccount, errorProvider)
 
     @Test
     fun `EXPECT DisplayOnly WHEN signer address is blank`() = runTest {
@@ -48,73 +48,55 @@ class CreateWalletConnectArbitraryDataSignerUseCaseTest {
     }
 
     @Test
-    fun `EXPECT Unsignable WHEN transaction signer is NoAuth`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns SignerNotFound.NoAuth(SIGNER_ADDRESS)
+    fun `EXPECT Signer with original address WHEN local account is Algo25`() = runTest {
+        coEvery { getLocalAccount(SIGNER_ADDRESS) } returns LocalAccount.Algo25(SIGNER_ADDRESS)
 
         val result = sut(SIGNER_ADDRESS)
 
-        assertTrue(result is WalletConnectArbitraryDataSigner.Unsignable)
-        assertEquals(MISSING_SIGNER_ERROR, result.error)
+        assertTrue(result is WalletConnectArbitraryDataSigner.Signer)
+        assertEquals(SIGNER_ADDRESS, result.address)
     }
 
     @Test
-    fun `EXPECT Unsignable WHEN transaction signer is Rekeyed`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns SignerNotFound.Rekeyed(SIGNER_ADDRESS)
+    fun `EXPECT Signer with original address WHEN local account is HdKey`() = runTest {
+        coEvery { getLocalAccount(SIGNER_ADDRESS) } returns LocalAccount.HdKey(
+            algoAddress = SIGNER_ADDRESS,
+            publicKey = byteArrayOf(),
+            seedId = 0,
+            account = 0,
+            change = 0,
+            keyIndex = 0,
+            derivationType = 0
+        )
 
         val result = sut(SIGNER_ADDRESS)
 
-        assertTrue(result is WalletConnectArbitraryDataSigner.Unsignable)
-        assertEquals(MISSING_SIGNER_ERROR, result.error)
+        assertTrue(result is WalletConnectArbitraryDataSigner.Signer)
+        assertEquals(SIGNER_ADDRESS, result.address)
     }
 
     @Test
-    fun `EXPECT Unsignable WHEN transaction signer is AccountNotFound`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns SignerNotFound.AccountNotFound(SIGNER_ADDRESS)
-
-        val result = sut(SIGNER_ADDRESS)
-
-        assertTrue(result is WalletConnectArbitraryDataSigner.Unsignable)
-        assertEquals(MISSING_SIGNER_ERROR, result.error)
-    }
-
-    @Test
-    fun `EXPECT Unsignable WHEN transaction signer is AuthAccountIsNoAuth`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns SignerNotFound.AuthAccountIsNoAuth(SIGNER_ADDRESS)
-
-        val result = sut(SIGNER_ADDRESS)
-
-        assertTrue(result is WalletConnectArbitraryDataSigner.Unsignable)
-        assertEquals(MISSING_SIGNER_ERROR, result.error)
-    }
-
-    @Test
-    fun `EXPECT Unsignable WHEN transaction signer is AuthAccountSigningDetailsNotFound`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns SignerNotFound.AuthAccountSigningDetailsNotFound(
-            address = SIGNER_ADDRESS,
-            authAddress = "authAddress"
+    fun `EXPECT Unsignable WHEN local account is Joint`() = runTest {
+        coEvery { getLocalAccount(SIGNER_ADDRESS) } returns LocalAccount.Joint(
+            algoAddress = SIGNER_ADDRESS,
+            participantAddresses = emptyList(),
+            threshold = 1,
+            version = 1
         )
 
         val result = sut(SIGNER_ADDRESS)
 
         assertTrue(result is WalletConnectArbitraryDataSigner.Unsignable)
-        assertEquals(MISSING_SIGNER_ERROR, result.error)
+        assertEquals(MULTISIG_TRANSACTION_ERROR, result.error)
     }
 
     @Test
-    fun `EXPECT Unsignable WHEN transaction signer is AuthAddressNotFound`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns SignerNotFound.AuthAddressNotFound(SIGNER_ADDRESS)
-
-        val result = sut(SIGNER_ADDRESS)
-
-        assertTrue(result is WalletConnectArbitraryDataSigner.Unsignable)
-        assertEquals(MISSING_SIGNER_ERROR, result.error)
-    }
-
-    @Test
-    fun `EXPECT Unsignable WHEN transaction signer is LedgerBLE`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns TransactionSigner.LedgerBle(
-            address = SIGNER_ADDRESS,
-            bluetoothAddress = "btAddress"
+    fun `EXPECT Unsignable WHEN local account is LedgerBle`() = runTest {
+        coEvery { getLocalAccount(SIGNER_ADDRESS) } returns LocalAccount.LedgerBle(
+            algoAddress = SIGNER_ADDRESS,
+            deviceMacAddress = "btAddress",
+            bluetoothName = null,
+            indexInLedger = 0
         )
 
         val result = sut(SIGNER_ADDRESS)
@@ -124,27 +106,29 @@ class CreateWalletConnectArbitraryDataSignerUseCaseTest {
     }
 
     @Test
-    fun `EXPECT Signer WHEN transaction signer is Algo25`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns TransactionSigner.Algo25(AUTH_ADDRESS)
+    fun `EXPECT Unsignable WHEN local account is NoAuth`() = runTest {
+        coEvery { getLocalAccount(SIGNER_ADDRESS) } returns LocalAccount.NoAuth(SIGNER_ADDRESS)
 
         val result = sut(SIGNER_ADDRESS)
 
-        assertEquals(AUTH_ADDRESS, result.address)
+        assertTrue(result is WalletConnectArbitraryDataSigner.Unsignable)
+        assertEquals(MISSING_SIGNER_ERROR, result.error)
     }
 
     @Test
-    fun `EXPECT Signer WHEN transaction signer is HdKey`() = runTest {
-        coEvery { getTransactionSigner(SIGNER_ADDRESS) } returns TransactionSigner.HdKey(AUTH_ADDRESS)
+    fun `EXPECT Unsignable WHEN local account is not found`() = runTest {
+        coEvery { getLocalAccount(SIGNER_ADDRESS) } returns null
 
         val result = sut(SIGNER_ADDRESS)
 
-        assertEquals(AUTH_ADDRESS, result.address)
+        assertTrue(result is WalletConnectArbitraryDataSigner.Unsignable)
+        assertEquals(MISSING_SIGNER_ERROR, result.error)
     }
 
     private companion object {
         private const val SIGNER_ADDRESS = "ADDRESS"
-        private const val AUTH_ADDRESS = "AUTH_ADDRESS"
         private val MISSING_SIGNER_ERROR = peraFixture<WalletConnectError>()
         private val UNABLE_TO_SIGN_ERROR = peraFixture<WalletConnectError>()
+        private val MULTISIG_TRANSACTION_ERROR = peraFixture<WalletConnectError>()
     }
 }
